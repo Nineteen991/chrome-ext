03be9f697c61b72121be1955d765b21d230533f3
Nineteen991 <Nineteen99@gmail.com>
2022-08-15 22:08:27 -0700

    just started setting up the unsplash api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import { useEffect } from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
 export default function App() {
+  // don't use my unsplash access key
+  const accessKey = "ITbWLmIlf-GE-svaxX4EHLdbAyQV1YsWcp3uyNYSGO8"
+  const photosUrl = `https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${accessKey}`
+  // get backgrounds from unsplash api
+  useEffect(() => {
+    fetch(photosUrl)
+      .then(res => {
+        if (!res.ok) {
+          throw Error("Unsplash api fetch failed")
+        }
+        return res.json()
+      })
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
-    <h1>howdy</h1>
+    <Container></Container>
   )
 }
---
+++ src/components/CryptoData.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const CryptoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export default function CryptoData() {
+  return (
+      <CryptoDiv></CryptoDiv>
+  )
+}
